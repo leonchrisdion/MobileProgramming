597efeff0c6bfb9564c6dc9b8fcf3403af0bf5b8
Leon Chrisdion <chrisdion_l@yahoo.com>
2019-08-29 09:26:49 +0700

    Mobile Programming

---
+++ LifeCycle/app/src/main/java/com/github/leonchrisdion/lifecycle/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.github.leonchrisdion.lifecycle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.content.Intent
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+    val TAG="Main Activity"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d(TAG,"In Oncreate")
+        val about_us = findViewById<Button>(R.id.about_us)
+        about_us.setOnClickListener{
+            val i = Intent (this,AboutUs::class.java)
+            startActivity(i)
+        }
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG,"In Onstart")
+    }
+
+    override fun onStop(){
+        super.onStop()
+        Log.d(TAG,"In Onstop")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG,"In Onpause")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG,"In Ondestroy")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG,"In OnRestart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG,"In Onresume")
+    }
+}
